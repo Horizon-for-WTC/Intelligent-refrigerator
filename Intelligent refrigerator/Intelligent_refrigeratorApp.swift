6aed561f3b2f0ec837046e464ac29a0d290361e6
RemiliaScarlet <a1342966673@gmail.com>
2021-01-26 11:15:37 +0800

    Initial Commit

---
+++ Intelligent refrigerator/Intelligent_refrigeratorApp.swift
@@ -0,0 +1,20 @@
+//
+//  Intelligent_refrigeratorApp.swift
+//  Intelligent refrigerator
+//
+//  Created by RemiliaScarlet on 2021/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Intelligent_refrigeratorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
